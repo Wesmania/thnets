c8f2fa3ab2154a0daea9b8ea41f85a14ecacdfec
Igor Kotrasinski <ik345186@students.mimuw.edu.pl>
2016-12-03 00:03:11 +0100

    Properly batch and debatch tensor in Inception
    Signed-off-by: Igor Kotrasinski <ik345186@students.mimuw.edu.pl>

--- modules/Inception.c
+++ modules/Inception.c
@@ -17,21 +17,27 @@ int nnload_Inception(struct module *mod, struct nnmodule *n)
 THFloatTensor *nn_Inception_updateOutput(struct module *module, THFloatTensor *input)
 {
 	int batchdim = input->nDimension + 1;
+	int batched = 0;
 	THFloatTensor *output = module->output;
 	long newdims[batchdim];
 	struct module *modules = module->Inception.modules;
 
 	/* batch */
-	memcpy(newdims + 1, input->size, input->nDimension * sizeof(*input->size));
-	newdims[0] = 1;
-	THFloatTensor_resize(input, newdims, batchdim);
+	if (input->nDimension == 3) {
+		memcpy(newdims + 1, input->size, input->nDimension * sizeof(*input->size));
+		newdims[0] = 1;
+		THFloatTensor_resize(input, newdims, batchdim);
+		batched = 1;
+	}
 
 	modules[0].updateOutput(&modules[0], input);
 	THFloatTensor_set(output, modules[0].output);
 
 	/* debatch */
-	memcpy(newdims, output->size, batchdim * sizeof(*input->size));
-	THFloatTensor_resize(output, newdims + 1, batchdim - 1);
+	if (batched) {
+		memcpy(newdims, output->size, batchdim * sizeof(*input->size));
+		THFloatTensor_resize(output, newdims + 1, batchdim - 1);
+	}
 
 	return output;
 }
